b324e279a78b775006a670e204cb264d7edb68e5
Silversmithe <alexadranly@gmail.com>
2020-04-02 16:46:49 -0700

    added basic audio engine functionality

--- src/__main__.py
+++ src/__main__.py
@@ -6,37 +6,39 @@ from objects.Signals import Signals
 from managers.CookingTimerManager import CookingTimerManager
 
 from engines.TimerEngine import TimerEngine
+from engines.AudioEngine import AudioEngine
 
 if __name__ == '__main__':
 
-    signals = Signals()
-    signals_lock = threading.Lock()
+    signals = (threading.Lock(), Signals())
+    timer_manager = (threading.Lock(), CookingTimerManager())
+    phrases = (threading.Lock(), [])
 
-    timer_manager = CookingTimerManager()
-    timer_manager_lock = threading.Lock()
+    audio_engine = AudioEngine(signals=signals, phrases=phrases)
+    timer_engine = TimerEngine(signals=signals, timer_manager=timer_manager, phrases=phrases)
 
-    timer = TimerEngine(signals=signals, manager=timer_manager, manager_lock=timer_manager_lock)
-    timer.start()
-
-    time.sleep(5)
-
-    with timer_manager_lock:
-        timer_manager.addTimer('potato', 60)
+    audio_engine.start()
+    timer_engine.start()
 
     time.sleep(5)
 
-    with timer_manager_lock:
-        timer_manager.addTimer('broccoli', 120)
+    with timer_manager[0]:
+        timer_manager[1].addTimer('potato', 5)
 
     time.sleep(5)
 
-    with timer_manager_lock:
-        timer_manager.addTimer('boiling water', 180)
+    with timer_manager[0]:
+        timer_manager[1].addTimer('broccoli', 10)
 
-    time.sleep(300)
+    time.sleep(5)
 
+    with timer_manager[0]:
+        timer_manager[1].addTimer('boiling water', 15)
 
-    with signals_lock:
-        signals.System_Running = False
+    time.sleep(100)
 
-    timer.join()
+    with timer_manager[0]:
+        timer_manager[1].System_Running = False
+
+    timer_engine.join()
+    audio_engine.join()
--- src/engines/AudioEngine.py
+++ src/engines/AudioEngine.py
@@ -5,8 +5,35 @@
 import threading
 import time
 
+from gtts import gTTS
+import os
 
-class AudioEngine(object):
 
-    def __init__(self):
-        pass
+class AudioEngine(threading.Thread):
+
+    def __init__(self, signals, phrases):
+        threading.Thread.__init__(self)
+        self.__signals_lock = signals[0]
+        self.__signals = signals[1]
+
+        self.__phrases_lock = phrases[0]
+        self.__phrases = phrases[1]
+
+        self.__temp_output_file = 'temp.mp3'
+        self.__tts_output = None
+
+    def run(self):
+        while self.__signals.System_Running == True:
+
+            if self.__signals.AudioEngineActive == True:
+                with self.__phrases_lock:
+                    if len(self.__phrases) > 0:
+                        phrase = self.__phrases.pop(0)
+                        self.__tts_output = gTTS(phrase, 'en')
+                        self.__tts_output.save(self.__temp_output_file)
+                        os.system('mpg123 -q ' + self.__temp_output_file)
+                        time.sleep(1)
+        
+            time.sleep(0.5)
+
+        print('Audio Engine is Finished')
